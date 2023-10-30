5fd75853afe8de82fd4c456cb205222cbd14777b
mokarrom17 <mokarrombasher@gmail.com>
2023-10-30 15:25:20 +0600

    added JWT section

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // MiddleWare
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 
 
@@ -41,13 +46,20 @@ async function run() {
             const user = req.body;
             console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.send(token);
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                    sameSite: true
+                })
+                .send({ success: true });
         })
 
         // Bookings
 
         app.get('/bookings', async (req, res) => {
             console.log(req.query.email)
+            console.log('tttt token', req.cookies.token)
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
